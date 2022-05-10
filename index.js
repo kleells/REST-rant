a3b3abb28198ff8ff7d8a462bbfef547cfc73c87
Katie Eells <katieeells@Katies-MBP.lan>
2022-05-10 18:40:08 -0400

    Added 404 error route

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 // installed npm init -y and npm install express to create package.json, node_modules and package-lock.json
 
 const express = require('express');
@@ -7,4 +9,11 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.listen(3000);
+//WILDCARD Route(keep at the bottom to avoid override) with call of .status(404)
+app.get('*', (req,res) =>
+{
+    res.status(404).send('<h1>404 Error Page</h1>');
+})
+
+//PORT address in .env
+app.listen(process.env.PORT);
