62fb27b2ef4eab8b1723a0c9987be89a9503762e
Katie Eells <katieeells@Katies-MBP.lan>
2022-05-12 17:37:32 -0400

    edited README

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get('/', (req, res) => {
 //WILDCARD Route(keep at the bottom to avoid override) with call of .status(404)
 app.get('*', (req,res) =>
 {
-    res.status(404).send('<h1>404 Error Page</h1>');
+    res.status(404).send('<h1>404 Error. Please go back to previous page</h1>');
 })
 
 //PORT address in .env
