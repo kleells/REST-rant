cfbe8f3388f506c66670e8865592aa4ad031a7cc
Katie Eells <katieeells@Katies-MBP.lan>
2022-05-13 19:39:03 -0400

    added some CSS styling

--- index.js
+++ index.js
@@ -1,28 +1,24 @@
-require('dotenv').config();
+// Modules and Globals
+require('dotenv').config()
+const express = require('express')
+const app = express()
 
-// installed npm init -y and npm install express to create package.json, node_modules and package-lock.json
-
-const express = require('express');
-const app = express();
-
-app.set('view engine', 'jsx');
+// Express Settings
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
-// only homepage and wildcard routes here, the rest need to be in the controllers folder
-// imports contorollers routes here
-
+// Controllers & Routes
 app.use('/places', require('./controllers/places'))
 
-//HOMEPAGE
 app.get('/', (req, res) => {
-    res.render('Home');
-});
-
-//WILDCARD Route(keep at the bottom to avoid override) with call of .status(404)
-app.get('*', (req,res) =>
-{
-    res.status(404).render('error404');
+    res.render('home')
 })
 
-//PORT address in .env
-app.listen(process.env.PORT);
+app.get('*', (req, res) => {
+    res.render('error404')
+})
+
+// Listen for Connections
+app.listen(process.env.PORT)
--- views/default.jsx
+++ views/default.jsx
@@ -5,7 +5,9 @@ function Def (html) {
         <html>
             <head>
                 <title>Title</title>
-                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous" />
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" cross
+                rigin="anonymous" />
+                <link rel="stylesheet" href="/css/style.css"/>
             </head>
             <body>
                 {html.children}
