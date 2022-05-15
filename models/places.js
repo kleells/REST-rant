e8419995fa37832090c056837147c159acc4e1b2
Katie Eells <katieeells@Katies-MBP.lan>
2022-05-15 18:18:21 -0400

    adding new places function

---
+++ models/places.js
@@ -0,0 +1,13 @@
+module.exports = [{
+    name: 'Busters On 28th',
+    city: 'Minneapolis',
+    state: 'MN',
+    cuisines: 'American, Craft beer',
+    pic: '/images/craftBurger.jpeg'
+  }, {
+    name: 'Brewery Bhavana',
+    city: 'Raleigh',
+    state: 'NC',
+    cuisines: 'Chinese, Craft beer',
+    pic: '/images/dimSum.jpeg'
+  }]
