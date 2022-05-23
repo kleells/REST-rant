7fb7555844c3a48c179c17192a838379301a023e
Katie Eells <katieeells@Katies-MBP.lan>
2022-05-22 20:34:19 -0400

    added seeders, helper methods and validation

--- controllers/places.js
+++ controllers/places.js
@@ -13,30 +13,42 @@ router.get('/', (req, res) => {
       })
 })
 
+// Post NEW Place
 router.post('/', (req, res) => {
   db.Place.create(req.body)
   .then(() => {
       res.redirect('/places')
   })
   .catch(err => {
-      console.log('err', err)
-      res.render('error404')
+      if (err && err.name == 'ValidationError') {
+        let message = "Validation Error: "
+        for(var field in err.errors){
+          message+= `${field} was ${err.errors[field].value}.`
+          message+= `${err.errors[field].message}`
+        }
+        console.log('Validation errore message', message)
+        res.render('places/new', {message})
+      }
+      else {
+          res.render('error404')
+      }
   })
 })
+
 
 // GET /places/new
 router.get('/new', (req, res) => {
   res.render('places/new');
 });
 
-// GET /places/:id
+// GET /places show
 router.get('/:id', (req, res) => {
   res.send('GET /places/:id stub')
 })
 
 // PUT Route
 router.put('/:id', (req, res) => {
-  res.send('PUT /places/:id stub')
+  res.send('PUT /:id stub')
 })
 
 // DELETE /places/:id
