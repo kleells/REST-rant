720d4200603a053aa4088169d3bb20fbb4dfc6d5
Katie Eells <katieeells@katies-mbp.lan>
2022-06-05 16:20:18 -0400

    fixed json engines typo

--- views/default.jsx
+++ views/default.jsx
@@ -4,7 +4,7 @@ function Def (html) {
     return (
         <html>
             <head>
-                <title>Title</title>
+                <title>REST-Rant</title>
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossOrigin="anonymous" />
                 <link rel="stylesheet" href="/css/style.css"/>
             </head>
